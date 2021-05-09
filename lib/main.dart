a8e370f2b729d134d31264d572c17989dca19522
Vanessa <vanessa@vanessa>
2021-05-08 21:08:16 -0300

    alteracao

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Clicou :',
+              'Clicou aqui :',
             ),
             Text(
               '$_counter',
